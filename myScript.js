af73e8d4cfeed3505df0a4162152b3aefcf1ea76
Sonsawan <sonsawan.n1994@gmail.com>
2023-06-15 21:40:09 +0700

    update JS code

--- myScript.js
+++ myScript.js
@@ -18,11 +18,43 @@ const recordCollection = {
       albumTitle: 'ABBA Gold'
     }
   };
-  
   // Only change code below this line
   function updateRecords(records, id, prop, value) {
-    
-    return records;
+
+    if (value != "") {
+
+      // ID that have prop
+      if (records[id].hasOwnProperty(prop)) {
+        
+        if (prop == 'artist') {
+          console.log("This is an artist")
+        } 
+        else if (prop == 'tracks') {
+          console.log("This is a tracks ")
+      } 
+        else if (prop == 'albumTitle') {
+        console.log("This is a albumTitle ")
+    } 
+    }
+      // ID that not have prop 
+      else if (prop == 'tracks') {
+        console.log("This is not have an tracks")
+      }
+      else if (prop == 'artist') {
+        console.log("This is not have an artist")
+      } 
+      else if (prop == 'albumTitle') {
+        console.log("This is a albumTitle ")
+      } 
+
+    // Value == emtry
+    else {
+      console.log("what the hell ?")
+      // delete id.prop
+    }
+    // return records;
+    }
   }
   
-console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+// console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+updateRecords(recordCollection, 5439, 'albumTitle', '1')
