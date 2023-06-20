604a7a30396c2b43897b545852d050f144b62b3a
Sonsawan <sonsawan.n1994@gmail.com>
2023-06-20 21:48:12 +0700

    update script

--- myScript.js
+++ myScript.js
@@ -1,18 +1,17 @@
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"]
-};
-function makeList(arr) {
-  // Only change code below this line
-  const failureItems = arr.map(item => 
-    `<li class="text-warning">${item}</li>`
-  )
-  // Only change code above this line
-
-  return failureItems;
+function add2Num(a, b) {
+  let result = [];
+  for (let index = 0; index < a.length; index++) {
+    result[index] = a[index] + b[index];
+  }
+  for (let index = 0; index < result.length; index++) {
+    if (result[index] > 9) {
+      if (result[index] > 9) {
+        result[index] = result[index] % 10;
+        Math.floor(result[index + 1] + (result[index] /= 10));
+      } 
+    } 
+  }
+  return result
 }
 
-const failuresList = makeList(result.failure);
-
-console.log(failuresList)
+console.log(add2Num([2,12,7, 1],[5,12,6, 2]))
